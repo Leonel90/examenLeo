5634a164a8eb5cfb3367f291b44ff83ff4ca4729
Leonel Omar Sangolquiza Amancha <loa.sangolquiza@yavirac.edu.ec>
2024-05-21 00:28:07 -0500

    inicio del proyecto

---
+++ Leo/LeoSan/src/main/java/com/leonel/LeoSan/LeoSanApplication.java
@@ -0,0 +1,13 @@
+package com.leonel.LeoSan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeoSanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeoSanApplication.class, args);
+	}
+
+}
---
+++ Leo/LeoSan/src/test/java/com/leonel/LeoSan/LeoSanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leonel.LeoSan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeoSanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
